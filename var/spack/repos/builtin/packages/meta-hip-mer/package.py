dfd7a935dfb6a0ded1cfad62a8cf210992521ea4
Christian Heusel <christian@heusel.eu>
2020-12-31 00:39:17 +0100

    add package description
    Signed-off-by: Christian Heusel <christian@heusel.eu>

--- var/spack/repos/builtin/packages/meta-hip-mer/package.py
+++ var/spack/repos/builtin/packages/meta-hip-mer/package.py
@@ -3,28 +3,16 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install meta-hip-mer
-#
-# You can edit this file again by typing:
-#
-#     spack edit meta-hip-mer
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class MetaHipMer(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
+    """
+    MetaHipMer (MHM) is a de novo metagenome short-read assembler. This is
+    version 2 (MHM2), which is written entirely in UPC++ and runs efficiently
+    on both single servers and on multinode supercomputers, where it can scale
+    up to coassemble terabase-sized metagenomes.
+    """
 
     homepage = "https://bitbucket.org/berkeleylab/mhm2/src/e674623ebeb52bb0b0e8dbb979ba51aef0ddaa93/docs/mhm_guide.md"
     url = "https://bitbucket.org/berkeleylab/mhm2/downloads/mhm2-2.0.0.tar.gz"
